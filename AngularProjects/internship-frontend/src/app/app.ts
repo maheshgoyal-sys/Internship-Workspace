0dbf538a18e1dc9e22a425f0003bfaaf62327dda
maheshgoyal-sys <maheshgoyal20032004@gmail.com>
2026-06-17 22:03:43 +0530

    Added Angular and Laravel projects

---
+++ AngularProjects/internship-frontend/src/app/app.ts
@@ -0,0 +1,16 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, FooterComponent],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('internship-frontend');
+}
