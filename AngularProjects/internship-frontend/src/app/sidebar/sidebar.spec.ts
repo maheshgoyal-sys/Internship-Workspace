d438398ca8cd1fda94d0fa5aeaef641672170641
maheshgoyal-sys <maheshgoyal20032004@gmail.com>
2026-06-28 13:13:12 +0530

    Updated sidebar UI and fixes

--- AngularProjects/internship-frontend/src/app/sidebar/sidebar.spec.ts
+++ AngularProjects/internship-frontend/src/app/sidebar/sidebar.spec.ts
@@ -1,22 +1,47 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { SidebarComponent } from './sidebar';
 
-import { Sidebar } from './sidebar';
-
-describe('Sidebar', () => {
-  let component: Sidebar;
-  let fixture: ComponentFixture<Sidebar>;
+describe('SidebarComponent', () => {
+  let component: SidebarComponent;
+  let fixture: ComponentFixture<SidebarComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Sidebar],
+      imports: [SidebarComponent],
+      providers: [provideRouter([])]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(Sidebar);
+    fixture = TestBed.createComponent(SidebarComponent);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should toggle sidebar open state', () => {
+    expect(component.isOpen).toBeFalsy();
+
+    component.toggleSidebar();
+
+    expect(component.isOpen).toBeTruthy();
+  });
+
+  it('should close sidebar', () => {
+    component.isOpen = true;
+
+    component.closeSidebar();
+
+    expect(component.isOpen).toBeFalsy();
+  });
+
+  it('should toggle tickets accordion', () => {
+    expect(component.ticketsExpanded).toBeFalsy();
+
+    component.toggleTickets();
+
+    expect(component.ticketsExpanded).toBeTruthy();
   });
 });
